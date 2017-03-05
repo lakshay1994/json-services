65769b5fa9a99f5be8a0c736462de14b08d05da9
lakshay1994 <lakshaykhurana1994@gmail.com>
2017-03-05 12:29:35 +0530

    initial changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cors = require('cors');
 
 var app = express();
 app.use(cors());
-
+app.set('port', (process.env.PORT || 5000));
 app.get('/sliderImgUrl', function(req, res) {
     res.json({
         images: [{
@@ -19,4 +19,6 @@ app.get('/sliderImgUrl', function(req, res) {
     });
 });
 
-app.listen(3001);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
