8ed37bad93e2dfe21c3e44bec8a4723f74d4fd56
lakshay1994 <lakshaykhurana1994@gmail.com>
2017-03-06 00:00:53 +0530

    db2

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('', {results: result.rows} ); }
+       { response.render('/', {results: result.rows} ); }
     });
   });
 });
