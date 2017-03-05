721b7b45e075a4dfd7db6092f334e2144c795cd2
lakshay1994 <lakshaykhurana1994@gmail.com>
2017-03-05 16:57:38 +0530

    shirt json added

--- index.js
+++ index.js
@@ -19,6 +19,62 @@ app.get('/sliderImgUrl', function(req, res) {
     });
 });
 
+app.get('/getShirtThumbs', function(req, res) {
+    res.json({
+        images: [{
+            id: 1, 
+            url: 'https://image.ibb.co/cFhOka/1.jpg',
+            shirtName: 'Name1',
+            shirtPrice: '450' 
+        }, {
+            id: 2, 
+            url: 'https://image.ibb.co/hP7Cdv/2.jpg',
+            shirtName: 'Name2',
+            shirtPrice: '850' 
+        }, {
+            id: 3, 
+            url: 'https://image.ibb.co/m2nOka/3.jpg',
+            shirtName: 'Name3',
+            shirtPrice: '550' 
+        }, {
+            id: 4, 
+            url: 'https://image.ibb.co/jz6G5a/4.jpg',
+            shirtName: 'Name4',
+            shirtPrice: '499' 
+        }, {
+            id: 5, 
+            url: 'https://image.ibb.co/gvgSBF/5.jpg',
+            shirtName: 'Name5',
+            shirtPrice: '410' 
+        }, {
+            id: 6, 
+            url: 'https://image.ibb.co/bQP3ka/6.jpg',
+            shirtName: 'Name6',
+            shirtPrice: '450' 
+        }, {
+            id: 7, 
+            url: 'https://image.ibb.co/goW5yv/7.jpg',
+            shirtName: 'Name7',
+            shirtPrice: '850' 
+        }, {
+            id: 8, 
+            url: 'https://image.ibb.co/koUErF/8.jpg',
+            shirtName: 'Name8',
+            shirtPrice: '650' 
+        }, {
+            id: 9, 
+            url: 'https://image.ibb.co/bTiw5a/9.jpg',
+            shirtName: 'Name9',
+            shirtPrice: '599' 
+        }, {
+            id: 10,
+            url: 'https://image.ibb.co/iHRG5a/10.jpg',
+            shirtName: 'Name10',
+            shirtPrice: '899' 
+        }] 
+    });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
