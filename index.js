9426e75ee8cee7b8399c07e64698cf1f2923e50c
lakshay1994 <lakshaykhurana1994@gmail.com>
2017-03-08 00:23:18 +0530

    images thumbs from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var pg = require('pg');
 var app = express();
 app.use(cors());
 app.set('port', (process.env.PORT || 5000));
@@ -18,61 +19,61 @@ app.get('/sliderImgUrl', function(req, res) {
     });
 });
 
-app.get('/getShirtThumbs', function(req, res) {
-    res.json({
-        images: [{
-            id: 1, 
-            url: 'https://image.ibb.co/cFhOka/1.jpg',
-            shirtName: 'Name1',
-            shirtPrice: '450' 
-        }, {
-            id: 2, 
-            url: 'https://image.ibb.co/hP7Cdv/2.jpg',
-            shirtName: 'Name2',
-            shirtPrice: '850' 
-        }, {
-            id: 3, 
-            url: 'https://image.ibb.co/m2nOka/3.jpg',
-            shirtName: 'Name3',
-            shirtPrice: '550' 
-        }, {
-            id: 4, 
-            url: 'https://image.ibb.co/jz6G5a/4.jpg',
-            shirtName: 'Name4',
-            shirtPrice: '499' 
-        }, {
-            id: 5, 
-            url: 'https://image.ibb.co/gvgSBF/5.jpg',
-            shirtName: 'Name5',
-            shirtPrice: '410' 
-        }, {
-            id: 6, 
-            url: 'https://image.ibb.co/bQP3ka/6.jpg',
-            shirtName: 'Name6',
-            shirtPrice: '450' 
-        }, {
-            id: 7, 
-            url: 'https://image.ibb.co/goW5yv/7.jpg',
-            shirtName: 'Name7',
-            shirtPrice: '850' 
-        }, {
-            id: 8, 
-            url: 'https://image.ibb.co/koUErF/8.jpg',
-            shirtName: 'Name8',
-            shirtPrice: '650' 
-        }, {
-            id: 9, 
-            url: 'https://image.ibb.co/bTiw5a/9.jpg',
-            shirtName: 'Name9',
-            shirtPrice: '599' 
-        }, {
-            id: 10,
-            url: 'https://image.ibb.co/iHRG5a/10.jpg',
-            shirtName: 'Name10',
-            shirtPrice: '899' 
-        }] 
-    });
-});
+// app.get('/getShirtThumbs', function(req, res) {
+//     res.json({
+//         images: [{
+//             id: 1, 
+//             url: 'https://image.ibb.co/cFhOka/1.jpg',
+//             shirtName: 'Name1',
+//             shirtPrice: '450' 
+//         }, {
+//             id: 2, 
+//             url: 'https://image.ibb.co/hP7Cdv/2.jpg',
+//             shirtName: 'Name2',
+//             shirtPrice: '850' 
+//         }, {
+//             id: 3, 
+//             url: 'https://image.ibb.co/m2nOka/3.jpg',
+//             shirtName: 'Name3',
+//             shirtPrice: '550' 
+//         }, {
+//             id: 4, 
+//             url: 'https://image.ibb.co/jz6G5a/4.jpg',
+//             shirtName: 'Name4',
+//             shirtPrice: '499' 
+//         }, {
+//             id: 5, 
+//             url: 'https://image.ibb.co/gvgSBF/5.jpg',
+//             shirtName: 'Name5',
+//             shirtPrice: '410' 
+//         }, {
+//             id: 6, 
+//             url: 'https://image.ibb.co/bQP3ka/6.jpg',
+//             shirtName: 'Name6',
+//             shirtPrice: '450' 
+//         }, {
+//             id: 7, 
+//             url: 'https://image.ibb.co/goW5yv/7.jpg',
+//             shirtName: 'Name7',
+//             shirtPrice: '850' 
+//         }, {
+//             id: 8, 
+//             url: 'https://image.ibb.co/koUErF/8.jpg',
+//             shirtName: 'Name8',
+//             shirtPrice: '650' 
+//         }, {
+//             id: 9, 
+//             url: 'https://image.ibb.co/bTiw5a/9.jpg',
+//             shirtName: 'Name9',
+//             shirtPrice: '599' 
+//         }, {
+//             id: 10,
+//             url: 'https://image.ibb.co/iHRG5a/10.jpg',
+//             shirtName: 'Name10',
+//             shirtPrice: '899' 
+//         }] 
+//     });
+// });
 
 app.get('/getShirt', function(req, res) {
     res.json({
@@ -83,7 +84,7 @@ app.get('/getShirt', function(req, res) {
     });
 });
 
-var pg = require('pg');
+
 // app.get('/getDB', function(req, res) {
 //     pg.defaults.ssl = true;
 //     pg.connect(process.env.DATABASE_URL, function(err, client) {
@@ -110,6 +111,19 @@ app.get('/getDB', function (request, response) {
        { console.error(err); response.send("Error " + err); }
       else
        { response.json(result.rows ); }
+    });
+  });
+});
+
+app.get('/getShirtThumbs', function (request, response) {
+    pg.defaults.ssl = true;
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT shirt_id, shirt_price, shirt_thumb FROM shirts', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.json( result.rows ); }
     });
   });
 });
