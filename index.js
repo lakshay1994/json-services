a75ba27fcd2bb67b0d2a4749c02575e8c9cfe633
lakshay1994 <lakshaykhurana1994@gmail.com>
2017-03-07 09:31:30 +0530

    df

--- index.js
+++ index.js
@@ -102,7 +102,8 @@ var pg = require('pg');
 app.get('/getDB', function (request, response) {
     pg.defaults.ssl = true;
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM check123 where id=1;', function(err, result) {
+      var shirt_id = request.get('shirt_id');
+    client.query('SELECT * FROM shirts where shirt_id=' + shirt_id, function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
@@ -115,3 +116,7 @@ app.get('/getDB', function (request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+// create table shirts(shirt_id integer, shirt_name text, shirt_description text, shirt_price integer, shirt_thumb text, shirt_img1 text, shirt_img2 text, shirt_img3 text, shirt_img4 text)
+// insert into shirts values(1, 'Red checks', 'Jack & Jones OR YORK SHIRT L/S IN' , 1299, 'https://image.ibb.co/hP7Cdv/2.jpg', 'https://image.ibb.co/cyTtJv/b.jpg', 'https://image.ibb.co/khCyka/a.jpg', 'https://image.ibb.co/krgDJv/c.jpg', NULL)
